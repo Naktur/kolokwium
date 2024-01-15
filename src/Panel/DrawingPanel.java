7629a22a78d2152f8665696651b9f63eb967233a
Naktur <gierlak.artur@gmail.com>
2024-01-15 16:02:20 +0100

    Third commit (half time)

--- src/Panel/DrawingPanel.java
+++ src/Panel/DrawingPanel.java
@@ -17,10 +17,42 @@ public class DrawingPanel extends JPanel {
         this.setPreferredSize(new Dimension(600,600));
         this.setFocusable(true);
 
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                startX=e.getX();
+                startY=e.getY();
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                endX=e.getX();
+                endY=e.getY();
+
+                Rectangle newRectangle=new Rectangle(startX,startY,endX-startX,endY-startY);
+                rectangleThreadsList.add(new RectangleThread(newRectangle, DrawingPanel.this));
+                rectangleThreadsList.get(rectangleThreadsList.size()-1).start();
+            }
+        });
+
     }
 
     public void paint(Graphics g) {
-        super.paintComponent(g);
+
+        if(rectangleThreadsList.size()!=0)
+        {
+            for (RectangleThread rectangleThread : rectangleThreadsList) {
+                Rectangle rectangle = rectangleThread.rectangle;
+                g.setColor(Color.BLUE);
+                g.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+            }
+        }
+
+
+
 
 
     }
