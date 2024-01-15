25b8a09a65d837e345ff63dde160264cdc3387ba
Naktur <gierlak.artur@gmail.com>
2024-01-15 15:37:56 +0100

    Second commit

--- src/Panel/DrawingPanel.java
+++ src/Panel/DrawingPanel.java
@@ -7,12 +7,21 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DrawingPanel extends JPanel {
+    private List<RectangleThread> rectangleThreadsList;
+    private int startX, startY, endX, endY;
     DrawingPanel(){
-
+        rectangleThreadsList = new ArrayList<>();
         this.setPreferredSize(new Dimension(600,600));
         this.setFocusable(true);
+
+    }
+
+    public void paint(Graphics g) {
+        super.paintComponent(g);
+
 
     }
 }
--- src/Threads/RectangleThread.java
+++ src/Threads/RectangleThread.java
@@ -1,4 +1,31 @@
 package Threads;
 
-public class RectangleThread {
+import java.awt.*;
+import Panel.DrawingPanel;
+
+public class RectangleThread extends Thread{
+    public Rectangle rectangle;
+    private DrawingPanel drawingPanel;
+    private int move=5;
+
+    public RectangleThread(Rectangle rectangle, DrawingPanel drawingPanel) {
+        this.rectangle = rectangle;
+        this.drawingPanel = drawingPanel;
+    }
+
+    @Override
+    public void run(){
+        while(true){
+            rectangle.y+=move;
+            drawingPanel.repaint();
+
+
+            try {
+                Thread.sleep(50);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
